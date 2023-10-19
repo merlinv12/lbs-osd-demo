66eaa1e4da560e576ef5d81039ae7f52913b7881
Anish Patel <anish.patel@leicabiosystems.com>
2023-10-19 11:49:50 -0700

    clean up

--- server/sharp.js
+++ server/sharp.js
@@ -1,28 +1,23 @@
 const sharp = require('sharp');
 const path = require('path');
 
-
+let image = './uploads/sample1.svs'
 const convertToDZI = (image) => {
 let fullFileName = path.basename(image);
 let extenstion = path.extname(image);
 let fileName = path.basename(fullFileName, extenstion);
 console.log(image)
-sharp(image, {limitInputPixels: false})
-    .png()
+sharp(image, {limitInputPixels: false, page: 0})
+    .jpeg()
     .tile({
         size: 1024
     })
-    .toBuffer(function(err, data, info) {
-      console.log(err) 
-      console.log(data)
-      console.log(info)
-    })
-  .toFile(`${fileName}.dz`, function(err, info) {
+  .toFile(`./dz/${fileName}.dz`, function(err, info) {
     // output.dzi is the Deep Zoom XML definition
     // output_files contains 512x512 tiles grouped by zoom level
     console.log(err)
   });
 }
 
-
+convertToDZI(image)
 
